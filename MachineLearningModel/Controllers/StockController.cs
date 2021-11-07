7ce889c200e849d6ebd3682d74a685ecd716d8c6
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-11-07 10:56:11 +0200

    Basic API structure

---
+++ MachineLearningModel/Controllers/StockController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MachineLearningModel.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : ControllerBase
+    {
+        private static readonly string[] StockNames = GetStockNames();
+
+        private static string[] GetStockNames()
+        {
+            var fileNames = Directory.GetFiles(".\\Data\\stocks");
+            var stockNames = new string[fileNames.Length];
+            var len = 0;
+            foreach (var fileName in fileNames)
+            {
+                var stock = fileName.Substring(".\\Data\\stocks\\".Length);
+                stock = stock.Replace(".csv", "");
+                stockNames[len++] = stock;
+            }
+
+            return stockNames;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetAllStocks()
+        {
+            return StockNames;
+        }
+    }
+}
